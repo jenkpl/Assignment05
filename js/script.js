032879b330453262827bd74d5e612ef393746b77
jenkpl <jenkpl@gmail.com>
2021-03-18 12:09:42 -0700

    trying something else

--- js/script.js
+++ js/script.js
@@ -8,13 +8,19 @@ empForm.addEventListener('submit', addEmployee);{
 }
     e.preventDefault();
 
-  
+    //let ccSelection = ccForm.ccselect.value;
+    //console.log(ccSelection);
 
     const idU = document.getElementById('id');
     const nameU = document.getElementById('name');
     const extU = document.getElementById('ext');
     const emailU = document.getElementById('email');
     const deptU = document.getElementById('dept');
+let enroll = getElementById('id');
+    enroll.tellName(e) => {
+        e.addEventListener('blur', something)
+    alert( `Hello, nameU, idU`);
+}
 
     console.log(`ID:`, idU);
     console.log(`Name:`, nameU);
